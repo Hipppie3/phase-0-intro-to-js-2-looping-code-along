4c376ead6387b184897edd5b8caa35f3095f6176
Daniel Hiep Truong <danieltruong89@yahoo.com>
2022-09-25 18:37:47 -0600

    commit tree 74a2f62b747cf1a60895146dbdf5e0c440b4d8dd

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+
+
+function writeCards(array, event) {
+ let newArray = []
+ for (let i = 0; i < array.length; i++) {
+  newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)}
+ return newArray;
+}
+console.log(writeCards(["Charlie", "Samip","Ali"], "birthday" ))
+
+function countDown(number){
+while (number > 0) {
+ console.log(number);
+ number -= 1;
+}
+console.log(number);
+}
